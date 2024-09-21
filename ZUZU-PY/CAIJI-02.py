62eed618dc51ee8504d8d8c2c1ed2bd05804b2ab
pdj996 <95870943+pengdejiang@users.noreply.github.com>
2024-09-22 01:40:02 +0800

    Update CAIJI-02.py

--- ZUZU-PY/CAIJI-02.py
+++ ZUZU-PY/CAIJI-02.py
@@ -6,6 +6,12 @@ import cv2
 import datetime
 from datetime import datetime
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+
+# 初始化变量来保存上一次解析的HTML内容和连续相同次数的计数器
+last_html_content = None
+consecutive_same_count = 0
 
 # 获取rtp目录下的文件名
 files = os.listdir('rtp-02')
@@ -17,11 +23,11 @@ for file in files:
     name, extension = os.path.splitext(file)
     files_name.append(name)
 
-# 忽略不符合要求的文件名
+#忽略不符合要求的文件名
 provinces_isps = [name for name in files_name if name.count('_') == 1]
 
 # 打印结果
-print(f"本次查询：{provinces_isps}的组播节目")
+print(f"本次查询：{provinces_isps}的组播节目") 
 
 keywords = []
 
@@ -38,53 +44,73 @@ for province_isp in provinces_isps:
                 mcast = first_line.split("rtp://")[1].split(" ")[0]
                 keywords.append(province_isp + "_" + mcast)
     except FileNotFoundError:
+    # 如果文件不存在，则捕获 FileNotFoundError 异常并打印提示信息
         print(f"文件 '{province_isp}.txt' 不存在. 跳过此文件.")
 
 for keyword in keywords:
     province, isp, mcast = keyword.split("_")
+    #将省份转成英文小写
     # 根据不同的 isp 设置不同的 org 值
     if province == "北京" and isp == "联通":
+        isp_en = "cucc"
         org = "China Unicom Beijing Province Network"
     elif isp == "联通":
+        isp_en = "cucc"
         org = "CHINA UNICOM China169 Backbone"
     elif isp == "电信":
         org = "Chinanet"
+        isp_en = "ctcc"
     elif isp == "移动":
         org = "China Mobile communications corporation"
+        isp_en = "cmcc"
+        
+#    else:
+#        org = ""
 
-    current_time = datetime.now()  # 获取当前时间
-    timeout_cnt = 0   # 初始化超时计数器
-    result_urls = set()  # 初始化一个集合来存储结果URL
-    while len(result_urls) == 0 and timeout_cnt <= 5:  # 循环直到找到结果URL或超时次数超过5次
+    current_time = datetime.now()
+    timeout_cnt = 0
+    result_urls = set() 
+    while len(result_urls) == 0 and timeout_cnt <= 5:
         try:
-            # 构造搜索URL的基础部分			
             search_url = 'https://fofa.info/result?qbase64='
-			# 构造搜索文本，包括目标名称、国家和区域
             search_txt = f'\"Rozhuk\" && country=\"CN\" && region=\"{province}\"'
-			# 将搜索文本编码为UTF-8，然后转换为base64编码
-            bytes_string = search_txt.encode('utf-8')			
+                # 将字符串编码为字节流
+            bytes_string = search_txt.encode('utf-8')
+                # 使用 base64 进行编码
             search_txt = base64.b64encode(bytes_string).decode('utf-8')
-			# 将base64编码的搜索文本添加到搜索URL中
             search_url += search_txt
-			# 打印当前时间和搜索URL
             print(f"{current_time} 查询运营商 : {province}{isp} ，查询网址 : {search_url}")
-			# 发送GET请求到搜索URL
-            response = requests.get(search_url, timeout=30)
-			# 检查请求是否成功
+            response = requests.get(search_url, timeout=5)
+            # 处理响应
             response.raise_for_status()
-			# 获取响应的HTML内容
+            # 检查请求是否成功
             html_content = response.text
-			# 使用BeautifulSoup解析HTML内容
+            # 使用BeautifulSoup解析网页内容
             html_soup = BeautifulSoup(html_content, "html.parser")
-			# 定义正则表达式来查找IP地址和端口
+
+            # 检查是否连续20次返回相同内容
+            if last_html_content is not None and html_content == last_html_content:
+                consecutive_same_count += 1
+            else:
+                consecutive_same_count = 0
+
+            # 如果连续20次相同，停止所有任务并跳出循环
+            if consecutive_same_count == 10:
+                print("连续10次返回相同内容，停止所有任务并跳出循环。")
+                break  # 跳出while循环
+
+            # 保存当前解析的HTML内容
+            last_html_content = html_content
+			
+            # print(f"{current_time} html_content:{html_content}")
+            # 查找所有符合指定格式的网址
+            # 设置匹配的格式，如http://8.8.8.8:8888
             pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
-			# 使用正则表达式查找所有匹配的URL
             urls_all = re.findall(pattern, html_content)
-			# 将找到的URL转换为集合，去除重复项
+            # 去重得到唯一的URL列表
             result_urls = set(urls_all)
-			# 打印当前时间和找到的URL集合
             print(f"{current_time} result_urls:{result_urls}")
-            # 初始化一个列表来存储有效的IP地址
+
             valid_ips = []
 
             # 遍历所有视频链接
@@ -97,47 +123,36 @@ for keyword in keywords:
                 # 检查视频是否成功打开
                 if not cap.isOpened():
                     print(f"{current_time} {video_url} 无效")
-                    continue  # Skip to the next URL
-
-                # 读取视频的宽度和高度
-                width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-				# 打印视频URL和分辨率
-                print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
-                # 如果视频的宽度和高度都大于0，则认为该IP地址有效
-                if width > 0 and height > 0:
-                    valid_ips.append(url)
-				# 释放VideoCapture对象	
-                cap.release()
-                # 如果找到了有效的IP地址
+                else:
+                    # 读取视频的宽度和高度
+                    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
+                    # 检查分辨率是否大于0
+                    if width > 0 and height > 0:
+                        valid_ips.append(url)
+                    # 关闭视频流
+                    cap.release()
+                    
             if valid_ips:
-				# 读取现有的RTP文件内容
-                rtp_filename = f'rtp/{province}_{isp}.txt'
+                #生成节目列表 省份运营商.txt
+                rtp_filename = f'rtp/{province}_{isp}CJ-02.txt'
                 with open(rtp_filename, 'r', encoding='utf-8') as file:
                     data = file.read()
-				# 构造新的文件名	
-                txt_filename = f'{province}{isp}CJ-02.txt'
-				# 写入新的文件内容，替换RTP URL
+                txt_filename = f'{province}{isp}.txt'
                 with open(txt_filename, 'w') as new_file:
                     for url in valid_ips:
                         new_data = data.replace("rtp://", f"{url}/rtp/")
                         new_file.write(new_data)
-                # 打印生成的文件名和保存位置
+
                 print(f'已生成播放列表，保存至{txt_filename}')
-            else:
-				# 如果没有找到有效的IP地址
-                print(f"未找到合适的 IP 地址。")
-       # 尝试捕获两种异常：requests.Timeout 和 requests.RequestException
+
         except (requests.Timeout, requests.RequestException) as e:
-			# 如果发生超时或请求异常，增加超时计数器
             timeout_cnt += 1
-			# 打印当前时间和发生超时的省份，以及异常次数
             print(f"{current_time} [{province}]搜索请求发生超时，异常次数：{timeout_cnt}")
-			# 判断超时次数是否超过5次
-            if timeout_cnt <= 5:
-		    # 如果没有超过5次，则继续执行循环，尝试重新发送请求
+            if timeout_cnt <= 3:
+                    # 继续下一次循环迭代
                 continue
             else:
-			# 如果超过5次，则打印错误信息并停止处理	
                 print(f"{current_time} 搜索IPTV频道源[]，超时次数过多：{timeout_cnt} 次，停止处理")
 print('节目表制作完成！ 文件输出在当前文件夹！')
