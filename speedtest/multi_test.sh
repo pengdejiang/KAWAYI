691d67b7d3554612084ed1b0e74968cc50de6d04
pdj996 <95870943+pengdejiang@users.noreply.github.com>
2024-10-10 22:55:39 +0800

    Update multi_test.sh

--- speedtest/multi_test.sh
+++ speedtest/multi_test.sh
@@ -6,21 +6,21 @@ i=0
 
 if [ $# -eq 0 ]; then
   echo "请选择城市："
-#  echo "1. 上海电信（Shanghai_103）"
-#  echo "2. 北京联通（Beijing_liantong_145）"
-#  echo "3. 四川电信（Sichuan_333）"
-#  echo "4. 浙江电信（Zhejiang_120）"
-#  echo "5. 北京电信（Beijing_dianxin_186）"
-#  echo "6. 揭阳酒店（Jieyang_129）"
-#  echo "7. 广东电信（Guangdong_332）"
-#  echo "8. 河南电信（Henan_327）"
-#  echo "9. 山西电信（Shanxi_117）"
-#  echo "10. 天津联通（Tianjin_160）"
-#  echo "11. 湖北电信（Hubei_90）"
-#  echo "12. 福建电信（Fujian_114）"
-#  echo "13. 湖南电信（Hunan_282）"
-#  echo "14. 甘肃电信（Gansu_105）"
-#  echo "1. 河北联通（Hebei_313）"
+  echo "1. 上海电信（Shanghai_103）"
+  echo "2. 北京联通（Beijing_liantong_145）"
+  echo "3. 四川电信（Sichuan_333）"
+  echo "4. 浙江电信（Zhejiang_120）"
+  echo "5. 北京电信（Beijing_dianxin_186）"
+  echo "6. 揭阳酒店（Jieyang_129）"
+  echo "7. 广东电信（Guangdong_332）"
+  echo "8. 河南电信（Henan_327）"
+  echo "9. 山西电信（Shanxi_117）"
+  echo "10. 天津联通（Tianjin_160）"
+  echo "11. 湖北电信（Hubei_90）"
+  echo "12. 福建电信（Fujian_114）"
+  echo "13. 湖南电信（Hunan_282）"
+  echo "14. 甘肃电信（Gansu_105）"
+  echo "15. 河北联通（Hebei_313）"
   echo "0. 全部"
   read -t 10 -p "输入选择或在10秒内无输入将默认选择全部: " city_choice
 
@@ -33,79 +33,78 @@ else
   city_choice=$1
 fi
 
-
 # 根据用户选择设置城市和相应的stream
 case $city_choice in
-#    1)
-#        city="Shanghai_103"
-#        stream="udp/239.45.1.4:5140"
-#	channel_key="上海"
-#        ;;
-#    2)
-#        city="Beijing_liantong_145"
-#        stream="rtp/239.3.1.236:2000"
-#        channel_key="北京联通"
-#        ;;
-#    3)
-#        city="Sichuan_333"
-#        stream="udp/239.93.42.33:5140"
-#        channel_key="四川电信"
-#        ;;
-#    4)
-#        city="Zhejiang_120"
-#        stream="rtp/233.50.201.63:5140"
-#        channel_key="浙江电信"
-#        ;;
-#    5)
-#        city="Beijing_dianxin_186"
-#        stream="udp/225.1.8.80:2000"
-#        channel_key="北京电信"
-#        ;;
-#    6)
-#        city="Jieyang_129"
-#        stream="hls/38/index.m3u8"
-#        channel_key="揭西"
-#        ;;
-#    7)
-#        city="Guangdong_332"
-#        stream="udp/239.77.1.98:5146"
-#        channel_key="广东电信"
-#        ;;
-#    8)
-#        city="Henan_327"
-#        stream="rtp/239.16.20.1:10010"
-#        channel_key="河南电信"
-#        ;;
-#    9)
-#        city="Shanxi_117"
-#        stream="udp/239.1.1.7:8007"
-#        channel_key="山西电信"
-#        ;;
-#    10)
-#        city="Tianjin_160"
-#        stream="udp/225.1.2.190:5002"
-#        channel_key="天津联通"
-#        ;;
-#    11)
-#        city="Hubei_90"
-#        stream="rtp/239.69.1.141:10482"
-#        channel_key="湖北电信"
-#        ;;
-#    12)
-#        city="Fujian_114"
-#        stream="rtp/239.61.2.183:9086"
-#        channel_key="福建电信"
-#        ;;
-#    13)
-#        city="Hunan_282"
-#        stream="udp/239.76.252.35:9000"
-#        channel_key="湖南电信"
-#        ;;
-#    14)
-#        city="Gansu_105"
-#        stream="udp/239.255.30.123:8231"
-#        channel_key="甘肃电信"
-#        ;;
+    1)
+        city="Shanghai_103"
+        stream="udp/239.45.1.4:5140"
+	channel_key="上海"
+        ;;
+    2)
+        city="Beijing_liantong_145"
+        stream="rtp/239.3.1.236:2000"
+        channel_key="北京联通"
+        ;;
+    3)
+        city="Sichuan_333"
+        stream="udp/239.93.42.33:5140"
+        channel_key="四川电信"
+        ;;
+    4)
+        city="Zhejiang_120"
+        stream="rtp/233.50.201.63:5140"
+        channel_key="浙江电信"
+        ;;
+    5)
+        city="Beijing_dianxin_186"
+        stream="udp/225.1.8.80:2000"
+        channel_key="北京电信"
+        ;;
+    6)
+        city="Jieyang_129"
+        stream="hls/38/index.m3u8"
+        channel_key="揭西"
+        ;;
+    7)
+        city="Guangdong_332"
+        stream="udp/239.77.1.98:5146"
+        channel_key="广东电信"
+        ;;
+    8)
+        city="Henan_327"
+        stream="rtp/239.16.20.1:10010"
+        channel_key="河南电信"
+        ;;
+    9)
+        city="Shanxi_117"
+        stream="udp/239.1.1.7:8007"
+        channel_key="山西电信"
+        ;;
+    10)
+        city="Tianjin_160"
+        stream="udp/225.1.2.190:5002"
+        channel_key="天津联通"
+        ;;
+    11)
+        city="Hubei_90"
+        stream="rtp/239.69.1.141:10482"
+        channel_key="湖北电信"
+        ;;
+    12)
+        city="Fujian_114"
+        stream="rtp/239.61.2.183:9086"
+        channel_key="福建电信"
+        ;;
+    13)
+        city="Hunan_282"
+        stream="udp/239.76.252.35:9000"
+        channel_key="湖南电信"
+        ;;
+    14)
+        city="Gansu_105"
+        stream="udp/239.255.30.123:8231"
+        channel_key="甘肃电信"
+        ;;
     15)
         city="Hebei_313"
         stream="rtp/239.253.93.134:6631"
@@ -113,13 +112,8 @@ case $city_choice in
         ;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
-        
-        #这个是原版 选择1--15 注释掉
-#        for option in {1..15}; do
-        
-        # 直接对数字15执行操作 选择15号执行
-         option=15
-
+        #这里是关键核心选择 现在选择15-15 默认是选择1--15
+        for option in {15..15}; do
           bash  ./multi_test.sh $option  # 假定script_name.sh是当前脚本的文件名，$option将递归调用
         done
         exit 0
@@ -130,7 +124,6 @@ case $city_choice in
         exit 1
         ;;
 esac
-
 
 
 city="Hebei_313"
