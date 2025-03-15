c0b0b9ab0a6d38e37176c8d12a47029072baef66
pdj996 <95870943+pengdejiang@users.noreply.github.com>
2025-03-16 03:18:51 +0800

    Update push-ftp.py

--- push-ftp.py
+++ push-ftp.py
@@ -24,10 +24,6 @@ try:
     ftp.cwd('/github-rtp-jump')  # 切换到FTP服务器的指定目录
     logging.info(f'已切换到FTP服务器的目录: /github-jump')
 
-    # 设置本地目录以查找文件
-    local_directory = '.'  # 当前目录，你可以根据需要修改
-    logging.info(f'本地目录设置为: {local_directory}')
-
     # 遍历本地目录以查找所有名称包含特定字符串的文件
     patterns = {'*FFF.txt'}
     for filename in os.listdir(local_directory):
